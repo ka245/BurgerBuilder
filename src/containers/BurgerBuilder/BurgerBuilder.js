00e2dda7d8c659bc53f2c075c79e1fa74147b87f
ka245 <aiemacmh@gmail.com>
2019-03-04 15:04:26 +0600

    Passing Ingredients via Query Params. Navigated to the Contact Data Component. Order submission&Passing data btw pages. Orders page added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,30 +88,17 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        //alert('You continue!');
-        // this.setState( { loading: true} );
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Aiema Kuseinova',
-        //         address: {
-        //             street: 'Backstreet 1',
-        //             zipCode: '87654',
-        //             country: 'Kyrgyzstan'
-        //         },
-        //         email: 'test@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then( response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     }); 
-        this.props.history.push('/checkout');
+      
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render () {
